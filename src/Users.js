bc59b16eedc7c4a110a79cd2a2141139c49e39f9
YunPC <apsn2000@nate.com>
2021-09-02 19:40:11 +0900

    클릭하는 정보에 대해 요청하고 렌더링(#3)

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import useAsync from './useAsync'
+import User from './User'
 
 // LOADING, SUCCESS, ERROR
 async function getUsers() {
@@ -10,6 +11,7 @@ async function getUsers() {
 
 function Users() {
   const [state, refetch] = useAsync(getUsers, [], true)
+  const [userId, setUserId] = useState(null)
   const { loading, data: users, error } = state
   if (loading) return <div>로딩 중..</div>
   if (error) return <div>에러가 발생했습니다..</div>
@@ -19,12 +21,13 @@ function Users() {
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
       <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   )
 }
